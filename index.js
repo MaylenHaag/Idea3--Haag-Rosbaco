0c6fbee87694980d859ec1f023796201c4303e65
MaylenHaag <maylenhaag23@gmail.com>
2023-03-31 15:18:03 -0300

    correcciones entrega 2

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ let sexo;
 let kilometros;
 let categoria;
 
-let persona1;
 let sexoconf1 = false;
 let km = 0;
 let verificacion1;
@@ -17,76 +16,90 @@ const arrayParticipantes = [];
 let sexos;
 
 // participante 2
-let persona2;
 let sexoconf2 = false;
 let verificacion2;
 let verif2 = 0;
 
-// Funciones para crear los participantes
-function participante1() {
-  persona1 = {
-    nombre: prompt("Ingrese su nombre"),
-    apellido: prompt("Ingrese su apellido"),
-    dni: prompt("Ingrese su DNI"),
-    edad: parseInt(prompt("Ingrese su edad (sólo números)")),
-    sexo: prompt("Ingrese su sexo: femenino o masculino"),
-    kilometros: prompt(
-      "Ingrese el kilometraje que va a hacer: 43km, 65km o 95km"
-    ),
-  };
 
-  arrayParticipantes.push(persona1);
+// Clase para crear los participantes
+class Participante {
+  constructor(nombre, apellido, dni, edad, sexo, kilometros, categoria) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.dni = dni;
+    this.edad = edad;
+    this.sexo = sexo;
+    this.kilometros = kilometros;
+    this.categoria = categoria;
+
+  }
 }
 
-function participante2() {
-  persona2 = {
-    nombre: prompt("Ingrese el nombre del segundo participante"),
-    apellido: prompt("Ingrese el apellido"),
-    dni: prompt("Ingrese el DNI"),
-    edad: parseInt(prompt("Ingrese la edad (sólo números)")),
-    sexo: prompt("Ingrese el sexo: femenino o masculino"),
-  };
 
-  arrayParticipantes.push(persona2);
+//Funcion para mostrar informacion del participante
+function mostrarInformacionParticipante(Participante) {
+  alert(
+    `¡Gracias!
+        Verificá que los datos estén bien:
+        Nombre: ${Participante.nombre}
+        Apellido: ${Participante.apellido}
+        DNI: ${Participante.dni}
+        Edad: ${Participante.edad}
+        Sexo: ${Participante.sexo}
+        Kilometraje: ${km}km`
+  );
+
 }
 
-// Creamos participante 1 y corroboramos que toda la informacion este bien
-participante1();
 
+// Creamos participante 1
+const participante1 = new Participante(
+  nombre = prompt("Ingrese su nombre"),
+  apellido = prompt("Ingrese su apellido"),
+  dni = prompt("Ingrese su DNI"),
+  edad = parseInt(prompt("Ingrese su edad (sólo números)")),
+  sexo = prompt("Ingrese su sexo: femenino o masculino"),
+  kilometros = prompt(
+    "Ingrese el kilometraje que va a hacer: 43km, 65km o 95km"
+  )
+)
+arrayParticipantes.push(participante1);
+
+// Corroboramos que toda la informacion este bien
 while (sexoconf1 === false) {
-  if (persona1.sexo.toLowerCase() === "femenino") {
+  if (participante1.sexo.toLowerCase() === "femenino") {
     sexoconf1 = 1;
-  } else if (persona1.sexo.toLowerCase() === "masculino") {
+  } else if (participante1.sexo.toLowerCase() === "masculino") {
     sexoconf1 = 2;
   } else {
-    persona1.sexo = prompt("Ingrese el sexo nuevamente");
+    participante1.sexo = prompt("Ingrese el sexo nuevamente");
     sexoconf1 = false;
   }
 }
 
 while (km === 0) {
   if (
-    persona1.kilometros === "43km" ||
-    persona1.kilometros === 43 ||
-    persona1.kilometros === "43"
+    participante1.kilometros === "43km" ||
+    participante1.kilometros === 43 ||
+    participante1.kilometros === "43"
   ) {
     km = 43;
   } else if (
-    persona1.kilometros === "65km" ||
-    persona1.kilometros === 65 ||
-    persona1.kilometros === "65"
+    participante1.kilometros === "65km" ||
+    participante1.kilometros === 65 ||
+    participante1.kilometros === "65"
   ) {
     km = 65;
   } else if (
-    persona1.kilometros === "95km" ||
-    persona1.kilometros === 95 ||
-    persona1.kilometros === "95"
+    participante1.kilometros === "95km" ||
+    participante1.kilometros === 95 ||
+    participante1.kilometros === "95"
   ) {
     km = 95;
   } else {
     km = 0;
 
-    persona1.kilometros = prompt(
+    participante1.kilometros = prompt(
       "Ingrese el kilimetraje que va a hacer: 43km, 65km o 95km"
     );
   }
@@ -94,19 +107,10 @@ while (km === 0) {
   console.log(km);
 }
 
-if (persona1.edad < 10 || persona1.edad > 85) {
+if (participante1.edad < 10 || participante1.edad > 85) {
   alert("Usted no puede participar porque no cumple con la edad requerida.");
 } else {
-  alert(
-    `¡Gracias!
-        Verificá que los datos estén bien:
-        Nombre: ${persona1.nombre}
-        Apellido: ${persona1.apellido}
-        DNI: ${persona1.dni}
-        Edad: ${persona1.edad}
-        Sexo: ${persona1.sexo}
-        Kilometraje: ${km}km`
-  );
+  mostrarInformacionParticipante(participante1);
 
   verificacion1 = prompt("Ingrese SI o NO:");
 }
@@ -122,33 +126,34 @@ do {
       switch (anadirpersona.toLowerCase()) {
         // Creamos participante 2 y corroboramos que toda la informacion este bien
         case "si":
-          participante2();
+          
+          const participante2 = new Participante(
+            nombre = prompt("Ingrese su nombre"),
+            apellido = prompt("Ingrese su apellido"),
+            dni = prompt("Ingrese su DNI"),
+            edad = parseInt(prompt("Ingrese su edad (sólo números)")),
+            sexo = prompt("Ingrese su sexo: femenino o masculino"),
+        
+          )
+          arrayParticipantes.push(participante2);
 
           while (sexoconf2 === false) {
-            if (persona2.sexo.toLowerCase() === "femenino") {
+            if (participante2.sexo.toLowerCase() === "femenino") {
               sexoconf2 = 1;
-            } else if (persona2.sexo.toLowerCase() === "masculino") {
+            } else if (participante2.sexo.toLowerCase() === "masculino") {
               sexoconf2 = 2;
             } else {
-              persona2.sexo = prompt("Ingrese el sexo nuevamente");
+              participante2.sexo = prompt("Ingrese el sexo nuevamente");
               sexoconf2 = false;
             }
           }
 
-          if (persona2.edad < 10 || persona2.edad > 85) {
+          if (participante2.edad < 10 || participante2.edad > 85) {
             alert(
               "Usted no puede participar porque no cumple con la edad requerida."
             );
           } else {
-            alert(
-              `¡Gracias!
-                      Verificá que los datos estén bien:
-                      Nombre: ${persona2.nombre}
-                      Apellido: ${persona2.apellido}
-                      DNI: ${persona2.dni}
-                      Edad: ${persona2.edad}
-                      Sexo: ${persona2.sexo}`
-            );
+            mostrarInformacionParticipante(participante2);
 
             verificacion2 = prompt("Ingrese SI o NO:");
           }
@@ -157,21 +162,12 @@ do {
             if (verificacion2.toLowerCase() === "si") {
               categoriaDuplas();
               alert(
-                `¡Gracias! ${persona1.nombre} ${persona1.apellido} y ${persona2.nombre} ${persona2.apellido}. Ustedes han sido inscriptos en la categoria ${categoria} ${km}km`
+                `¡Gracias! ${participante1.nombre} ${participante1.apellido} y ${participante2.nombre} ${participante2.apellido}. Ustedes han sido inscriptos en la categoria ${categoria} ${km}km`
               );
               verif2 = 1;
             } else if (verificacion2.toLowerCase() === "no") {
               alert("Por favor vuelva a ingresar sus datos");
-              participante2();
-              alert(
-                `¡Gracias!
-                      Verificá que tus datos estén bien:
-                      Nombre: ${persona2.nombre}
-                      Apellido: ${persona2.apellido}
-                      DNI: ${persona2.dni}
-                      Edad: ${persona2.edad}
-                      Sexo: ${persona2.sexo}`
-              );
+              mostrarInformacionParticipante(participante2);
               verificacion2 = prompt("Ingrese SI o NO:");
             }
           } while (verif2 === 0);
@@ -181,7 +177,7 @@ do {
         case "no":
           categoriaSimple();
           alert(
-            `¡Gracias! ${persona1.nombre} ${persona1.apellido}. Usted ha sido inscripto en la categoria ${categoria} ${km}km`
+            `¡Gracias! ${participante1.nombre} ${participante1.apellido}. Usted ha sido inscripto en la categoria ${categoria} ${km}km`
           );
           break;
 
@@ -199,21 +195,12 @@ do {
     } else if (km === 43 || km === 65) {
       categoriaSimple();
       alert(
-        `¡Gracias! ${persona1.nombre} ${persona1.apellido}. Usted ha sido inscripto en la categoria ${categoria} ${km}km`
+        `¡Gracias! ${participante1.nombre} ${participante1.apellido}. Usted ha sido inscripto en la categoria ${categoria} ${km}km`
       );
     }
   } else if (verificacion1.toLowerCase() === "no") {
     alert("Por favor vuelva a ingresar sus datos");
-    participante1();
-    alert(
-      `¡Gracias!
-      Verificá que tus datos estén bien:
-      Nombre: ${persona1.nombre}
-      Apellido: ${persona1.apellido}
-      DNI: ${persona1.dni}
-      Edad: ${persona1.edad}
-      Sexo: ${persona1.sexo}`
-    );
+    mostrarInformacionParticipante(participante1);
     verificacion1 = prompt("Ingrese SI o NO:");
   }
 } while (verif1 === 0);
@@ -224,33 +211,33 @@ function categoriaSimple() {
     case 43:
       switch (sexoconf1) {
         case 1:
-          if (persona1.edad <= 16) {
+          if (participante1.edad <= 16) {
             categoria = "CICLO DAMAS JUVENILES";
-          } else if (persona1.edad > 16 && persona1.edad <= 35) {
+          } else if (participante1.edad > 16 && participante1.edad <= 35) {
             categoria = "CICLO DAMAS A";
-          } else if (persona1.edad >= 36 && persona1.edad < 45) {
+          } else if (participante1.edad >= 36 && participante1.edad < 45) {
             categoria = "CICLO DAMAS B";
-          } else if (persona1.edad >= 46 && persona1.edad < 55) {
+          } else if (participante1.edad >= 46 && participante1.edad < 55) {
             categoria = "CICLO DAMAS C";
-          } else if (persona1.edad >= 55 && persona1.edad < 65) {
+          } else if (participante1.edad >= 55 && participante1.edad < 65) {
             categoria = "CICLO DAMAS D";
-          } else if (persona1.edad >= 66) {
+          } else if (participante1.edad >= 66) {
             categoria = "CICLO DAMAS E";
           }
           break;
 
         case 2:
-          if (persona1.edad <= 16) {
+          if (participante1.edad <= 16) {
             categoria = "CICLO JUVENIL";
-          } else if (persona1.edad > 16 && persona1.edad <= 35) {
+          } else if (participante1.edad > 16 && participante1.edad <= 35) {
             categoria = "CICLOTURISMO A";
-          } else if (persona1.edad >= 36 && persona1.edad < 45) {
+          } else if (participante1.edad >= 36 && participante1.edad < 45) {
             categoria = "CICLOTURISMO B";
-          } else if (persona1.edad >= 46 && persona1.edad < 55) {
+          } else if (participante1.edad >= 46 && participante1.edad < 55) {
             categoria = "CICLOTURISMO C";
-          } else if (persona1.edad >= 55 && persona1.edad < 65) {
+          } else if (participante1.edad >= 55 && participante1.edad < 65) {
             categoria = "CICLOTURISMO D";
-          } else if (persona1.edad >= 66) {
+          } else if (participante1.edad >= 66) {
             categoria = "CICLOTURISMO E";
           }
           break;
@@ -260,33 +247,33 @@ function categoriaSimple() {
     case 65:
       switch (sexoconf1) {
         case 1:
-          if (persona1.edad >= 16 && persona1.edad <= 17) {
+          if (participante1.edad >= 16 && participante1.edad <= 17) {
             categoria = "DAMAS JUVENILES";
-          } else if (persona1.edad > 17 && persona1.edad <= 35) {
+          } else if (participante1.edad > 17 && participante1.edad <= 35) {
             categoria = "DAMAS A";
-          } else if (persona1.edad >= 36 && persona1.edad < 45) {
+          } else if (participante1.edad >= 36 && participante1.edad < 45) {
             categoria = "DAMAS B";
-          } else if (persona1.edad >= 46 && persona1.edad < 55) {
+          } else if (participante1.edad >= 46 && participante1.edad < 55) {
             categoria = "DAMAS C";
-          } else if (persona1.edad >= 55 && persona1.edad < 65) {
+          } else if (participante1.edad >= 55 && participante1.edad < 65) {
             categoria = "DAMAS D";
-          } else if (persona1.edad >= 66) {
+          } else if (participante1.edad >= 66) {
             categoria = "DAMAS E";
           }
           break;
 
         case 2:
-          if (persona1.edad <= 16) {
+          if (participante1.edad <= 16) {
             categoria = "JUVENILES";
-          } else if (persona1.edad > 16 && persona1.edad <= 35) {
+          } else if (participante1.edad > 16 && participante1.edad <= 35) {
             categoria = "MASTER A";
-          } else if (persona1.edad >= 36 && persona1.edad < 45) {
+          } else if (participante1.edad >= 36 && participante1.edad < 45) {
             categoria = "MASTER B";
-          } else if (persona1.edad >= 46 && persona1.edad < 55) {
+          } else if (participante1.edad >= 46 && participante1.edad < 55) {
             categoria = "MASTER C";
-          } else if (persona1.edad >= 55 && persona1.edad < 65) {
+          } else if (participante1.edad >= 55 && participante1.edad < 65) {
             categoria = "MASTER D";
-          } else if (persona1.edad >= 66) {
+          } else if (participante1.edad >= 66) {
             categoria = "MASTER E";
           }
           break;
@@ -296,41 +283,41 @@ function categoriaSimple() {
     case 95:
       switch (sexoconf1) {
         case 1:
-          if (persona1.edad >= 18 && persona1.edad < 29) {
+          if (participante1.edad >= 18 && participante1.edad < 29) {
             categoria = "DAMAS ELITE";
-          } else if (persona1.edad >= 30 && persona1.edad < 39) {
+          } else if (participante1.edad >= 30 && participante1.edad < 39) {
             categoria = "DAMAS A";
-          } else if (persona1.edad >= 40 && persona1.edad < 49) {
+          } else if (participante1.edad >= 40 && participante1.edad < 49) {
             categoria = "DAMAS B";
-          } else if (persona1.edad >= 50 && persona1.edad < 59) {
+          } else if (participante1.edad >= 50 && participante1.edad < 59) {
             categoria = "DAMAS C";
-          } else if (persona1.edad >= 60) {
+          } else if (participante1.edad >= 60) {
             categoria = "DAMAS D";
           }
           break;
 
         case 2:
-          if (persona1.edad >= 18 && persona1.edad < 23) {
+          if (participante1.edad >= 18 && participante1.edad < 23) {
             categoria = "SUB 23";
-          } else if (persona1.edad >= 24 && persona1.edad < 29) {
+          } else if (participante1.edad >= 24 && participante1.edad < 29) {
             categoria = "ELITE";
-          } else if (persona1.edad >= 30 && persona1.edad < 34) {
+          } else if (participante1.edad >= 30 && participante1.edad < 34) {
             categoria = "MASTER A1";
-          } else if (persona1.edad >= 35 && persona1.edad < 39) {
+          } else if (participante1.edad >= 35 && participante1.edad < 39) {
             categoria = "MASTER A2";
-          } else if (persona1.edad >= 40 && persona1.edad < 44) {
+          } else if (participante1.edad >= 40 && participante1.edad < 44) {
             categoria = "MASTER B1";
-          } else if (persona1.edad >= 45 && persona1.edad < 49) {
+          } else if (participante1.edad >= 45 && participante1.edad < 49) {
             categoria = "MASTER B2";
-          } else if (persona1.edad >= 50 && persona1.edad < 54) {
+          } else if (participante1.edad >= 50 && participante1.edad < 54) {
             categoria = "MASTER C1";
-          } else if (persona1.edad >= 55 && persona1.edad < 59) {
+          } else if (participante1.edad >= 55 && participante1.edad < 59) {
             categoria = "MASTER C2";
-          } else if (persona1.edad >= 60 && persona1.edad < 64) {
+          } else if (participante1.edad >= 60 && participante1.edad < 64) {
             categoria = "MASTER D1";
-          } else if (persona1.edad >= 65 && persona1.edad < 69) {
+          } else if (participante1.edad >= 65 && participante1.edad < 69) {
             categoria = "MASTER D2";
-          } else if (persona1.edad >= 70) {
+          } else if (participante1.edad >= 70) {
             categoria = "MASTER E";
           }
           break;
